0b24a597d23d21196d82c2e72a428f3d3aeee6eb
WeiXinqiao <31772367+WeiXinqiao@users.noreply.github.com>
2017-10-08 11:23:48 -0400

    connect the UI to Code part 1

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+
     }
 
-
-
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLable.text = "Default Text"
+    }
 }
 
